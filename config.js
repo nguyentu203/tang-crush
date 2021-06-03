ddebb9b93452d9e50fa3749b4b945efbfa1e0cb3
nguyentu203 <85296036+nguyentu203@users.noreply.github.com>
2021-06-04 06:45:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Em iu à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Quanh năm nước chảy đá mòn
+    Vẫn còn thời sự vẫn còn yêu em,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vẫn cứ là thích Tú <33',
     btnNo: 'Không, Cậu à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho Tú <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì cưới nha <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Yêu anh nhoaaaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' // https://www.facebook.com/thien.thien.21
+    messLink: 'http://fb.com' //https://www.facebook.com/thien.thien.21
 }
